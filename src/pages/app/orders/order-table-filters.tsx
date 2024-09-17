31635e6ec601d7e608c1573952157eb303a6c87b
Rafael dos Santos Batista <rafaeldsantos442@gmail.com>
2024-09-16 21:36:33 -0300

    feat: Realizado a criação da parte de filtros de orders

---
+++ src/pages/app/orders/order-table-filters.tsx
@@ -0,0 +1,44 @@
+import { Search, X } from 'lucide-react'
+
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select'
+
+const OrderTableFilters = () => {
+  return (
+    <form className="flex items-center gap-2">
+      <span className="text-sm font-semibold">Filtros:</span>
+      <Input placeholder="ID do pedido" className="h-8 w-auto" />
+      <Input placeholder="Nome do cliente" className="h-8 w-[320px]" />
+      <Select defaultValue="all">
+        <SelectTrigger className="h-8 w-[180px]">
+          <SelectValue />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectItem value="all">Todos status</SelectItem>
+          <SelectItem value="pending">Pendente</SelectItem>
+          <SelectItem value="canceled">Cancelado</SelectItem>
+          <SelectItem value="processing">Em preparo</SelectItem>
+          <SelectItem value="delivery">Em entrega</SelectItem>
+          <SelectItem value="delivered">Entegue</SelectItem>
+        </SelectContent>
+      </Select>
+      <Button variant={'secondary'} type="submit" size={'xs'}>
+        <Search className="mr-2 h-4 w-4" />
+        Filtrar resultados
+      </Button>
+      <Button variant={'outline'} type="button" size={'xs'}>
+        <X className="mr-2 h-4 w-4" />
+        Remover filtros
+      </Button>
+    </form>
+  )
+}
+
+export default OrderTableFilters
